f2b959583d60039ee1a9555c741a8a894e7dfd23
bezerra9 <gabrielbezerragof@gmail.com>
2026-02-24 12:18:44 -0300

    add: EDA (EXPLORE)

--- teo_ml/churn/train.py
+++ teo_ml/churn/train.py
@@ -1,5 +1,9 @@
 # %%
 import pandas as pd
+
+pd.options.display.max_columns = 500
+pd.options.display.max_rows = 500
+
 df = pd.read_csv('../data/abt_churn.csv')
 df.head()
 
@@ -21,6 +25,7 @@ X, y = df_train[features], df_train[target]
 
 
 # %%
+# SAMPLE
 from sklearn import model_selection
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, random_state=42, test_size=0.2, stratify=y)
 # estratificamos para manter a mesma proporção das taxas da celula abaixo
@@ -30,3 +35,38 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, random
 # %%
 print('Taxa de variavel resposta Treino', y_train.mean())
 print('Taxa de variavel resposta Teste', y_test.mean())
+
+
+# %%
+# EXPLORE (MISSINGS)
+X_train.isna().sum().sort_values(ascending=False)
+
+
+# %%
+df_analise = X_train.copy()
+df_analise[target] = y_train
+sumario = df_analise.groupby(by=target).agg(['mean', 'median']).T
+sumario
+
+
+# %%
+sumario['diff_abs'] = sumario[0] - sumario[1]
+sumario['diff_rel'] = sumario[0] / sumario[1]
+sumario.sort_values(by=['diff_rel'], ascending=False)
+# Média muito diferente da mediana é bom ficar de olho, é possivel que tenha uma distribuição esticada ou valores fora do comum
+
+
+# %%
+# Rodar uma árvore de decisão pode ser bom para explorar o dado (EXPLORE). Ajuda a entender quais variaveis estão nos ajudando
+from sklearn import tree
+import matplotlib.pyplot as plt
+
+arvore = tree.DecisionTreeClassifier(random_state=42)
+arvore.fit(X_train, y_train)
+
+
+# %%
+feature_importances = (pd.Series(arvore.feature_importances_, index=X_train.columns).sort_values(ascending=False).reset_index())
+
+feature_importances['acum.'] = feature_importances[0].cumsum()
+feature_importances[feature_importances[0]>0.01]
