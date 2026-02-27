8745834539dbbd687a162010978219ba6d12deee
bezerra9 <gabrielbezerragof@gmail.com>
2026-02-27 12:06:17 -0300

    add: adicionando ambiente virtual e detalhes

--- teo_ml/churn/train.py
+++ teo_ml/churn/train.py
@@ -101,54 +101,82 @@ onehot = encoding.OneHotEncoder(variables=best_features, ignore_format=True)
 # %%
 # Model
 from sklearn import linear_model
+from sklearn import naive_bayes
+from sklearn import ensemble
 
-reg = linear_model.LogisticRegression(penalty=None, max_iter=1000000, random_state=42)
+#model = naive_bayes.BernoulliNB() # Bernoulli pq minhas variaveis agora são todas onehot
+#model = linear_model.LogisticRegression(penalty=None, max_iter=1000000, random_state=42)
+model = ensemble.RandomForestClassifier(random_state=42, min_samples_leaf=20, n_jobs=-1, n_estimators=500)
 
 model_pipeline = pipeline.Pipeline(
   steps=[
     ('Discretizar', tree_discretization),
     ('Onehot', onehot),
-    ('Model', reg)
+    ('Model', model)
   ]
 )
 
-model_pipeline.fit(X_train, y_train)
+model_pipeline.fit(X_train[best_features], y_train)
 
-# X_train_transformado = model_pipeline[:-2].transform(X_train) -> somente para fins de conhecimento
+#X_train_transformado = model_pipeline[:-1].transform(X_train[best_features]) #-> somente para fins de conhecimento
 
 
 # %%
 from sklearn import metrics
 
-y_train_predict = model_pipeline.predict(X_train)
-y_train_proba = model_pipeline.predict_proba(X_train)[:, 1]
+
+# %%
+
+
+y_train_predict = model_pipeline.predict(X_train[best_features])
+y_train_proba = model_pipeline.predict_proba(X_train[best_features])[:, 1]
 
 acc_train = metrics.accuracy_score(y_train, y_train_predict)
 auc_train = metrics.roc_auc_score(y_train, y_train_proba)
+roc_train = metrics.roc_curve(y_train, y_train_proba)
 print('Acuracia Treino: ', acc_train)
 print('AUC Treino: ', auc_train)
 
 
 # %%
 
-y_test_predict = model_pipeline.predict(X_test)
-y_test_proba = model_pipeline.predict_proba(X_test)[:, 1]
+
+y_test_predict = model_pipeline.predict(X_test[best_features])
+y_test_proba = model_pipeline.predict_proba(X_test[best_features])[:, 1]
 
 acc_test = metrics.accuracy_score(y_test, y_test_predict)
 auc_test = metrics.roc_auc_score(y_test, y_test_proba)
+roc_test = metrics.roc_curve(y_test, y_test_proba)
+
 print('Acuracia teste: ', acc_test)
 print('AUC teste: ', auc_test)
 
 
 # %%
 
-y_oot_predict = model_pipeline.predict(oot[features])
-y_oot_proba = model_pipeline.predict_proba(oot[features])[:, 1]
+
+y_oot_predict = model_pipeline.predict(oot[best_features])
+y_oot_proba = model_pipeline.predict_proba(oot[best_features])[:, 1]
 
 acc_oot = metrics.accuracy_score(oot[target], y_oot_predict)
 auc_oot = metrics.roc_auc_score(oot[target], y_oot_proba)
+roc_oot = metrics.roc_curve(oot[target], y_oot_proba)
 print('Acuracia oot: ', acc_oot)
 print('AUC oot: ', auc_oot)
 
+
+# %%
+plt.plot(roc_train[0], roc_train[1])
+plt.plot(roc_test[0], roc_test[1])
+plt.plot(roc_oot[0], roc_oot[1])
+
+plt.grid(True)
+plt.title('Curva ROC')
+plt.legend([
+  f'Treino: {100*auc_train:.2f}',
+  f'Teste: {100*auc_test:.2f}',
+  f'Out of Time: {100*auc_oot:.2f}',
+])
+# a curva deve ficar proximas umas das outras e proximas de 100
 
 # %%
